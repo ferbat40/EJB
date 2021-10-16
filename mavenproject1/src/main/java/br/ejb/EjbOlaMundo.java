d87f67456b4250574c9d3427ff811a43eba49133
= <=>
2021-10-15 23:03:42 -0300

    alter ejb

--- mavenproject1/src/main/java/br/ejb/EjbOlaMundo.java
+++ mavenproject1/src/main/java/br/ejb/EjbOlaMundo.java
@@ -18,7 +18,7 @@ public class EjbOlaMundo {
     // "Insert Code > Add Business Method")
     
     public String getOie(){
-        return "oie EJB todo mundo usa";
-    }
+        return "oie EJB todo mundo usa porra";
+    } 
     
 }
